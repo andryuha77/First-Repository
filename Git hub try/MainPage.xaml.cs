1c99ea5ae57beaaf3b45ca6aae377294cf65dd7e
andryuha77 <andryuha1977@gmail.com>
2015-11-18 09:41:32 +0000

    Button

--- Git hub try/MainPage.xaml.cs
+++ Git hub try/MainPage.xaml.cs
@@ -16,6 +16,10 @@ using Windows.UI.Xaml.Navigation;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Git_hub_try
+
+    //addbutton
+    // add teext button
+    //cliick
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
